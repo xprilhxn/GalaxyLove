9baeb2038340d70d9c17ac0d7528315b6c2a46f5
xprilhxn <iadrian9990@gmail.com>
2025-07-22 23:38:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -819,7 +819,7 @@ let galaxyAudio = null;
 
 function preloadGalaxyAudio() {
   const audioSources = [
-   "AE THEM NHAC TUY NHA"
+  "audio/music.mp3"
   ];
 
   const randomIndex = Math.floor(Math.random() * audioSources.length);
